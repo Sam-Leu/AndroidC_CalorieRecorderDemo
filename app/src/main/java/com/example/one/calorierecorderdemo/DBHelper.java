39f8ebee9294a8c7974910c950fae1074aefa0f9
one <1161372240@qq.com>
2018-10-08 23:06:53 +0800

    version 2

---
+++ app/src/main/java/com/example/one/calorierecorderdemo/DBHelper.java
@@ -0,0 +1,39 @@
+package com.example.one.calorierecorderdemo;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DBname = "Calorie.db";
+    private static final String DAILY = "Daily";
+
+    private static final String CREATE_DAILY_TABLE
+            = "create table " + DAILY + "(id integer primary key autoincrement, date text, calorie integer)";
+
+    private static final String UPDATE_DAILY_TABLE
+            = "alter table " + DAILY + " add age integer";
+
+    public DBHelper(Context context, int version) {
+        super(context,DBname,null, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqlDb) {
+        sqlDb.execSQL(CREATE_DAILY_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        switch (oldVersion){
+            case 1:
+                db.execSQL(CREATE_DAILY_TABLE);
+                break;
+            default:
+                throw new IllegalStateException("Unkonw version:"+oldVersion);
+        }
+    }
+}
